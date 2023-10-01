9f2c8c5617fd835202ee89d07ff4e36e6e78282d
Jerricke <eejerrickw@gmail.com>
2023-10-01 12:33:46 -0600

    mern tutorial #5

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Workout = require('../models/workoutModel');
 
 const router =  express.Router()
 
@@ -13,8 +14,16 @@ router.get('/:id', (req, res) =>{
 })
 
 //POST 
-router.post('/', (req, res) =>{
-    res.json({ msg: 'POST workout'})
+router.post('/', async (req, res) =>{
+    const { title, load, reps} = req.body
+
+    try {
+        const workout = await Workout.create({title, load, reps})
+        res.status(200).json(workout)
+    }
+    catch (err) {
+        res.status(400).json({ error: err.message})
+    }
 })
 
 //DELETE
