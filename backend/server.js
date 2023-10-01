866b7954993ca8807fc3647cd2b87c6cd310b4b7
Jerricke <eejerrickw@gmail.com>
2023-10-01 12:23:19 -0600

    mern tutorial #4

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
 
 //instantiate express
@@ -16,7 +17,14 @@ app.use((req, res, next) => {
 //routes
 app.use('/api/workouts', workoutRoutes)
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('litsening on port 4000!!');
-})
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then( () => {
+        //listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('connected to DB and listening on port 4000!!');
+        })
+
+    })
+    .catch(err => console.log(err))
+    
