9b80e66cde8d895fd3395d28bb9992fedf9f73af
Jerricke <eejerrickw@gmail.com>
2023-10-01 13:30:39 -0600

    mern tutorial #9

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,33 @@
+import { useState, useEffect } from "react"
+import WorkoutDetails from "../components/WorkoutDetails"
 
 
 function Home() {
+
+  const [workouts, setWorkouts] = useState(null)
+
+  useEffect( () => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('/api/workouts')
+      console.log(response)
+      const json = await response.json()
+      if (response.ok) {
+        setWorkouts(json)
+      }
+    }
+
+    fetchWorkouts()
+  }, [])
+
   return (
-    <div>home</div>
+    <div className="home">
+      <div className="workouts">
+        {workouts && workouts.map(workout => (
+          <WorkoutDetails key={workout._id} workout={workout} />
+        ))
+        }
+      </div>
+    </div>
   )
 }
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+        "/api": "http://127.0.0.1:4000"
+          // // we can adjust the target based on our backend port
+          // target: "http://127.0.0.1:4000",
+          // changeOrigin:true,
+          // secure: false,
+          // rewrite: (path)=>path.replace(/^\/api/,"")
+      }
+  }
 })
