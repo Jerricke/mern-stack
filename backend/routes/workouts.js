af109502e75302ebe36f8be0700cfeea4c0e8618
Jerricke <eejerrickw@gmail.com>
2023-10-01 12:44:47 -0600

    mern #6 tutorial

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,30 +1,20 @@
 const express = require('express')
-const Workout = require('../models/workoutModel');
+const {
+    createWorkout,
+    getWorkouts,
+    getWorkout
+} = require('../controllers/workoutController')
 
 const router =  express.Router()
 
 //GET all
-router.get('/', (req, res) =>{
-    res.json({ msg: 'GET all workouts'})
-})
+router.get('/', getWorkouts)
 
 //GET by id
-router.get('/:id', (req, res) =>{
-    res.json({ msg: 'GET one workout by ID'})
-})
+router.get('/:id', getWorkout)
 
 //POST 
-router.post('/', async (req, res) =>{
-    const { title, load, reps} = req.body
-
-    try {
-        const workout = await Workout.create({title, load, reps})
-        res.status(200).json(workout)
-    }
-    catch (err) {
-        res.status(400).json({ error: err.message})
-    }
-})
+router.post('/', createWorkout)
 
 //DELETE
 router.delete('/:id', (req, res) =>{
