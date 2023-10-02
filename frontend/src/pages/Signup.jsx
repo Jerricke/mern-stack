432ffe0d3212c301a73e88a7e8c990d013e665c9
Jerricke <eejerrickw@gmail.com>
2023-10-02 17:38:51 -0600

    mern auth #10

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
+import { useSignup } from '../hooks/useSignup'
 
 const Signup = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const { signup, error, isLoading } = useSignup()
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        console.log({email, password})
+
+        await signup(email, password)
+    }
+
+    if (error) {
+        console.log(error)
     }
 
     return ( 
@@ -25,7 +32,8 @@ const Signup = () => {
                 onChange={e => setPassword(e.target.value)} 
                 value={password}
             />
-            <button type='submit'>Sign Up</button>
+            <button disabled={isLoading} type='submit'>Sign Up</button>
+            {error && <div className='error'>{error}</div>}
         </form>
     )
 }
