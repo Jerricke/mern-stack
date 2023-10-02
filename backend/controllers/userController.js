654dbf9f5f35dfab1df30e08f780dff598a1f1e8
Jerricke <eejerrickw@gmail.com>
2023-10-02 16:54:39 -0600

    mern auth #7

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,17 @@ const createToken = (_id) => {
 
 //login user
 const loginUser = async (req, res) => {
-    res.json({msg: "login user"}) ;
+    const { email, password } = req.body
+    try {
+        const user = await User.login(email, password)
+
+        // create token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token});
+    } catch (err) {
+        res.status(400).json({err: err.message});
+    }
 }
 
 //signup user
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -45,5 +45,28 @@ userSchema.statics.signup = async function(email, password){
     return user
 }
 
+// static login method
+userSchema.statics.login = async function(email, password) {
+
+     // check if email and password exist
+     if (!email || !password) {
+        throw Error('All fields must be filled.')
+    }
+
+    const user = await this.findOne({ email });
+
+    if (!user) {
+        throw Error('Email not registered.')
+    } 
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if (!match) {
+        throw Error("Incorrect password")
+    }
+
+    return user 
+}
+
 
 module.exports = mongoose.model('User', userSchema)
