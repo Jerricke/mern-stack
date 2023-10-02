ac358247827ace944f78283e23a262e1e9a5d401
Jerricke <eejerrickw@gmail.com>
2023-10-02 16:34:49 -0600

    mern auth tutorial #4

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -18,10 +19,21 @@ const userSchema = new Schema({
 // static signup method
 userSchema.statics.signup = async function(email, password){
 
+    //validation
+    if (!email || !password) {
+        throw Error('All fields must be filled.')
+    }
+    if (!validator.isEmail(email)) {
+        throw Error('Email is not valid')
+    }
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password is not strong enough')
+    }
+
     const exists = await this.findOne({ email });
 
     if (exists) {
-        throw Error('Email already exists')
+        throw Error('Email already exists.')
     }
 
     const salt = await bcrypt.genSalt(10) 
