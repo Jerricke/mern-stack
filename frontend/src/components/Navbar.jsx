71b76e561ec6e50179b4247eed0b38c82e8fc0dc
Jerricke <eejerrickw@gmail.com>
2023-10-04 15:15:36 -0600

    mern auth #11

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,12 @@
 import { Link } from "react-router-dom"
+import { useLogout } from "../hooks/useLogout";
+
 function Navbar() {
+  const { logout } = useLogout();
+
+  const handleClick = () => {
+    logout();
+  }
 
   return (
     <header>
@@ -8,6 +15,9 @@ function Navbar() {
                 <h1>Workout Buddy</h1>
             </Link>
             <nav>
+              <div>
+                <button onClick={handleClick}>Log out</button>
+              </div>
               <div>
                 <Link to="/login">Login</Link>
                 <Link to="/signup">Singup</Link>
